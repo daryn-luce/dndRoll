e8e5afd5637addb7e8342acd97052169181c1d13
Daryn Luce <daryn.luce@gmail.com>
2019-07-30 00:05:12 -0500

    updated to change dice sides/amount of rolls/ how many best of

--- darynDice.py
+++ darynDice.py
@@ -1,14 +1,14 @@
 import random
 
 # Rolls random number 1-6 
-def die():
-    return random.randint(1,6)
+def die(sides):
+    return random.randint(1,sides)
 
 # Rolls set of die how ever the amount returns list
-def roll(amount):
+def roll(amount,sides1,sides2):
     rollList = []
     for i in range(amount):
-        rollList.append([die(),die()])
+        rollList.append([die(sides1),die(sides2)])
     return rollList
 
 # Sorts list and returns first 3    
@@ -20,13 +20,18 @@ def bestOf(diceList,qty):
 def diceSum(diceList):
         return sum(diceList)
         
+amount_rolls = 4
+amount_best = 3
+die1_sides = 6
+die2_sides = 6
 dice = []
+
 print('Rolls')
-for die1,die2 in roll(4):
+for die1,die2 in roll(amount_rolls,die1_sides,die2_sides):
     dice.append(die1 + die2)
     print('{} and {} : {}'.format(die1,die2,(die1 + die2)))
 
-diceList = bestOf(dice,3)
+diceList = bestOf(dice,amount_best)
 
 print('-----')
 print('Best')
