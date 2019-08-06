8d5aedf0cd10ff2f25b46c77959de6ae45644452
Daryn Luce <daryn.luce@gmail.com>
2019-08-06 00:16:00 -0500

    uploaded daryns decoder

--- week3/darynDecode.py
+++ week3/darynDecode.py
@@ -1,4 +1,43 @@
-j = 'this is a string'
-words = j.split()
+def convert_char(char,key):
+    letter = ord(char)
+    letter += key
+    if letter > 122:
+        letter = ((letter - 122) + 96)
+    return chr(letter)
+    
+def decode_char(char,key):
+    letter = ord(char)
+    letter -= key
+    if letter < 97:
+        letter = ((letter - 97) + 123)
+    return chr(letter)
 
-print(words)
+def convert_message(message,key):
+    message = message.split()
+    codeword = list()
+    for word in message:
+        currentword = ''
+        for char in word:
+            currentword += convert_char(char,key)
+        codeword.append(currentword)
+    return ' '.join(codeword)
+    
+def decode_message(message,key):
+    message = message.split()
+    codeword = list()
+    for word in message:
+        currentword = ''
+        for char in word:
+            currentword += decode_char(char,key)
+        codeword.append(currentword)
+    return ' '.join(codeword)
+    
+    
+message = 'programming is fun i love you guys'
+key = 7
+
+coded_message = convert_message(message,key)
+print(coded_message, ':[coded message]')
+
+decoded_message = decode_message(coded_message,key)
+print(decoded_message, ':[decoded message]')
