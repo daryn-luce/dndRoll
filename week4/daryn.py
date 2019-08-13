97538c1892fe5c0d9bf3a908a74b715dd6291aff
Daryn Luce <daryn.luce@gmail.com>
2019-08-12 22:18:19 -0500

    calc

--- week4/daryn.py
+++ week4/daryn.py
@@ -1,1 +1,22 @@
-print('hi')
+def add_num(num1,num2):
+    return num1 + num2
+
+def sub_num(num1,num2):
+    return num1 - num2
+    
+def mult_num(num1,num2):
+    return num1 * num2
+    
+def div_num(num1,num2):
+    return num1 / num2
+
+def cube_num(num):
+    return num**3
+    
+def sqr_num(num):
+    return num**2
+
+def sqrt_num(num):
+    return num**(.5)
+    
+print(sqrt_num(81))
