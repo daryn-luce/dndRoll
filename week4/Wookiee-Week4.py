e9c01c22bc67bf9721b0dddec7ce20705642ea44
Wookiee <mitchhubbell87@gmail.com>
2019-08-14 10:59:45 -0500

    Added functionality

--- week4/Wookiee-Week4.py
+++ week4/Wookiee-Week4.py
@@ -3,18 +3,18 @@
 #print("Hey " + Daryn + " I Love You!")
 #print(f'Hey {Daryn} {69 * 420}')
 
-CalcFunctions = ["1. Addition", "2. Subtraction", "3. Multiplication", "4. Division"]
-CalcSymbols = ["+", "-", "*", "/"]
+CalcFunctions = ["1. Addition", "2. Subtraction", "3. Multiplication", "4. Division", "5. Squared", "6. Cubed"]
+CalcSymbols = ["+", "-", "*", "/", "²", "³"]
 
 def ask1():
     global n1
-    n1 = input("Please enter your first whole number")
+    n1 = input("Please enter a whole number.")
     if not n1.isdigit():
         return ask1()
 
 def ask2():
     global Method
-    print("Please choose a function by entering the corresponding number")
+    print("Please choose a function by entering the corresponding number.")
     print(*CalcFunctions, sep = "\n")
     Method = input()
     if not Method.isdigit():
@@ -22,12 +22,26 @@ def ask2():
 
 def ask3():
     global n2
-    n2 = input("Please enter your second whole number")
+    n2 = input("Please enter a second whole number.")
     if not n2.isdigit():
         return ask3()
 
 ask1()
 ask2()
+
+if Method == "5":
+    solve = int(n1) ** 2
+    print(n1, CalcSymbols[int(Method) - 1])
+    print("Your answer is:")
+    print(solve)
+    quit()
+if Method == "6":
+    solve = int(n1) ** 3
+    print(n1, CalcSymbols[int(Method) - 1])
+    print("Your answer is:")
+    print(solve)
+    quit()
+
 ask3()
 
 if Method == "1":
